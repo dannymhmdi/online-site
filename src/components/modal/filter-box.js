3a980eb4243b97622996be0977ae6d4fad58651a
dannymhmdi <dannymohammadi1377@gmail.com>
2023-07-20 11:27:38 +0330

    modal created for filter products

---
+++ src/components/modal/filter-box.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useState } from "react";
+import styles from "./modal-test.module.css";
+import Accordion from "react-bootstrap/Accordion";
+import { AccordionFilters } from "../accordion";
+const FilterBox = ({ onClose }) => {
+  // const [closeFilter, setCloseFilter] =useState(true)
+  return (
+    <Fragment>
+      <div className={`${styles["filter-box"]} col-12 px-3 col-lg-2`}>
+        <div className=" d-flex justify-content-between border-bottom p-3 col-12">
+          <h4>فیلتر</h4>
+          <button
+            className={`btn-close ${styles["filter-close-button"]}`}
+            onClick={onClose}
+          ></button>
+        </div>
+       <AccordionFilters/>
+      </div>
+    </Fragment>
+  );
+};
+
+export default FilterBox;
---
+++ src/components/modal/modal-test.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect, useState } from "react";
+import styles from "./modal-test.module.css";
+import Accordion from "react-bootstrap/Accordion";
+import { Row } from "react-bootstrap";
+import FilterBox from "./filter-box";
+import { useMediaQuery } from "@mui/material";
+import axios from "axios";
+const ModalTest = ({children}) => {
+  const [isShowfilter, setIsShowfilter] = useState(false);
+  const matches = useMediaQuery("(min-width:992px)");
+  const filterButtonHandler = () => {
+    setIsShowfilter(true);
+  };
+
+  const onCloseHandler = () => {
+    setIsShowfilter(false);
+  };
+
+  useEffect(() => {
+    if (matches) {
+      setIsShowfilter(false);
+    }
+  }, [matches]);
+
+
+  return (
+    <Fragment>
+      <div className={`mt-5 mb-2 ${styles['filter-btn-container']}`}>
+        <button
+          className={`d-flex btn btn-success  ${styles["filter-btn"]}`}
+          onClick={filterButtonHandler}
+        >
+          فیلتر
+          <span className="material-symbols-outlined p-1">tune</span>
+        </button>
+      </div>
+      <Row className="mt-lg-5">
+        {isShowfilter && <FilterBox onClose={onCloseHandler} />}
+
+        {matches && <FilterBox />}
+        <div className="product-items border border-1 border-danger col-12 col-lg-10 px-0s">
+         {children}
+        </div>
+      </Row>
+    </Fragment>
+  );
+};
+
+export default ModalTest;
