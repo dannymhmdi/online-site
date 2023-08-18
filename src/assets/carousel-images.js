5a524df444931816a09554c39480f7e410aec1ca
dannymhmdi <dannymohammadi1377@gmail.com>
2023-08-18 16:30:05 +0330

    carousel images added

--- src/assets/carousel-images.js
+++ src/assets/carousel-images.js
@@ -1,7 +1,6 @@
-import slide1 from "./slide1.png"
-import slide2 from "./slide2.jpg"
-import slide3 from "./slide3.jpg"
+import slide1 from "./slide1.png";
+import slide2 from "./slide2.jpg";
+import slide3 from "./slide3.jpg";
+export const images = [slide1, slide2, slide3];
 
-export const images = [slide1 , slide2 , slide3]
-
-export default images
+export default images;
