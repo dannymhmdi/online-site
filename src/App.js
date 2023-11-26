65393193410728b8f9bc0ea712a57395250e78af
dannymhmdi <dannymohammadi1377@gmail.com>
2023-11-26 09:31:36 +0330

    routes updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import MainLayout from "./Layout/main-layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Contact, Home, Products } from "./pages";
 import ShopBasket from "./pages/shop-basket";
+import SelectProduct from "./components/select-product/select-product";
+import { Information, Introduction, ProductCheck } from "./components/display-product";
 
 function App() {
   return (
@@ -13,6 +15,12 @@ function App() {
           <Route path="products" element={<Products />} />
           <Route path="contact-us" element={<Contact />} />
           <Route path="shop-basket" element={<ShopBasket/>}/>
+          {/* <Route path={`products/sneakers-model/:id/:model`} element={<SelectProduct/>}/> */}
+          <Route  path={`products/sneakers-model/:id/:model`} element={<SelectProduct/>}>
+            <Route path="introduction/:country" element={<Introduction/>}/>
+            <Route path="information" element={<Information/>}/>
+            <Route path="product-check" element={<ProductCheck/>}/>
+          </Route>
         </Routes>
       </MainLayout>
     </BrowserRouter>
